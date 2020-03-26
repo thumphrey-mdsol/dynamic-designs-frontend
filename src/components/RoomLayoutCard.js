2e1c687676c668bd463f4cd0ab29d58c441a6cd4
github username <github email address>
2020-03-26 15:01:32 -0400

    added stuff

--- src/components/RoomLayoutCard.js
+++ src/components/RoomLayoutCard.js
@@ -13,7 +13,7 @@ class RoomLayoutCard extends React.Component {
             <Card >
                 <div >
                 <Header as='h1'>{this.props.category} </Header> 
-                <Image src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSw_s2ZM9ORlp48nzcGqibvgw-2CsxT5Rbm3IQHkeUIIdg_lTXq" size='medium' />
+                <Image src={this.props.category} size='medium' />
                 <Divider hidden />
                 </div>
                 
